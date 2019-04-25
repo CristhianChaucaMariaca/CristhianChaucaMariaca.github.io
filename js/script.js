558a674e1a39337fcbc783f7411b05b3885a55e9
Cristhian Chauca <cristhian@localhost.localdomain>
2019-04-25 19:04:45 -0400

    se añadio el sector de habilidades..., con animación jQuery, se debe animar con efecto de desplazamiento de la ventana

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $(".inicio").delay('1000').fadeOut();
+    var html = $("#html-label").text();
+    $("#html").animate({'width':'0%'}).animate({"width":html},2000);
 });
