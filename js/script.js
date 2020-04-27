6dedf121ecac48150cf833c93a2abf359f6cf09d
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2020-04-26 21:11:03 -0400

    se modifico el menu para versión escritorio y se modificaron algunos detalles en el menu responsive

--- js/script.js
+++ js/script.js
@@ -149,7 +149,9 @@
 
 window.addEventListener('load', inicio);
 function inicio(){
+    ocultarInicio();
     document.getElementById('toggle-menu').addEventListener('click',mostrarMenu);
+    document.getElementById('togle-menu2').addEventListener('click', mostrarMunudesk);
 }
 function mostrarMenu(e){
     if(document.getElementById('nav').classList.toggle('mostrar-menu'))
@@ -159,5 +161,15 @@ function mostrarMenu(e){
     else{
         document.getElementById('toggle-menu').style.color="#00adb5"
     }
+}
+function mostrarMunudesk(){
+    document.getElementById('nav-ul').classList.toggle('mostrar-menu-desk');
+}
+function ocultarInicio(){
+    let inicio = document.getElementById('inicio');
+    inicio.style.transition= "opacity .3s easy all";
+    inicio.style.opacity= "0";
+    inicio.style.display = "none";
+
 }
 
